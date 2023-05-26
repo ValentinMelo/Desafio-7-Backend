8505cec1fb8d85460dbabe8098700ffa1c9aae50
Vale Melo <valemelo18@gmail.com>
2023-05-26 01:10:44 -0300

    modificaciones

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,63 +1,17 @@
 import express from 'express';
-import ProductManager from '../ProductManager.js';
+import ProductController from '../controllers/productController.js';
 
-const productManager = new ProductManager();
+const productController = new ProductController();
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const products = await productManager.getProducts();
-  res.render("index", { products });
-});
+router.get("/", productController.getAllProducts);
 
-router.get("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  try {
-    const product = await productManager.getProductById(productId);
-    if (!product) return res.status(404).send({ error: "Producto no encontrado" });
-    res.status(200).send(product);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Error interno' });
-  }
-});
+router.get("/:pid", productController.getProductById);
 
-router.post("/", async (req, res) => {
-  const { title, description, price, code, stock, category, thumbnails } = req.body;
-  const newProduct = await productManager.addProduct({
-    title,
-    description,
-    price,
-    code,
-    stock,
-    category,
-    thumbnails,
-    status: true
-  });
-  res.status(201).json(newProduct);
-});
+router.post("/", productController.addProduct);
 
-router.put("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  const { title, description, price, code, stock, category, thumbnails, status } = req.body;
-  const updatedProduct = await productManager.updateProduct(productId, {
-    title,
-    description,
-    price,
-    code,
-    stock,
-    category,
-    thumbnails,
-    status
-  });
-  if (!updatedProduct) return res.status(404).send({ error: "Producto no encontrado" });
-  res.status(200).send(updatedProduct);
-});
+router.put("/:pid", productController.updateProduct);
 
-router.delete("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  const deletedProduct = await productManager.deleteProduct(productId);
-  if (!deletedProduct) return res.status(404).send({ error: "Producto no encontrado" });
-  res.status(200).send(deletedProduct);
-});
+router.delete("/:pid", productController.deleteProduct);
 
 export default router;
